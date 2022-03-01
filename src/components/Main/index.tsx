b90cc1e187a95865c19724d4e039e030a6cfabfe
Gabriel <gabriel.merigo.dev@gmail.com>
2022-03-01 01:08:14 -0300

    more accs

--- src/components/Main/index.tsx
+++ src/components/Main/index.tsx
@@ -1,15 +1,16 @@
 import * as S from './styles'
 
-const Main = () => (
+const Main = ({
+  title = 'React Avançado',
+  description = 'Typescript, React JS, Next JS and Styled-Components'
+}) => (
   <S.Wrapper>
-    <S.Title>React Avançado</S.Title>
+    <S.Title>{title}</S.Title>
     <S.Logo
       src="/img/logo.svg"
       alt="Imagem de um átomo e React Avançado escrito ao lado"
     />
-    <S.Description>
-      Typescript, React JS, Next JS and Styled-Components
-    </S.Description>
+    <S.Description>{description}</S.Description>
     <S.Illustration
       src="/img/hero-illustration.svg"
       alt="Desenvolvedor de frente para tela com código"
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,10 @@
+// import { text, withKnobs } from `@storybook/addon-knobs`;
+import Main from '.';
+
+export default {
+  title: 'Main',
+  component: Main,
+  // decorators: [withKnobs]
+}
+
+export const Basic = () => <Main title={`teste`}/>
