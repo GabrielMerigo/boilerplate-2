d86bd5bf99de975dd997c395e2888a231b58290c
Gabriel <gabriel.merigo.dev@gmail.com>
2022-03-03 00:16:12 -0300

    more accs

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -11,6 +11,7 @@ function App({ Component, pageProps }: AppProps) {
         <link rel="shortcut icon" href="/img/icon-512.png" />
         <link rel="apple-touch-icon" href="/img/icon-512.png" />
         <meta name="description" content="A simple project" />
+        <link rel='manifest' href='/manifest.json' />
       </Head>
       <GlobalStyle />
       <Component {...pageProps} />
