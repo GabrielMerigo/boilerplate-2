9586c59b7c181845660b10b48f2e95b66ceedf0c
Gabriel Merigo <gabriel.merigo.dev@gmail.com>
2022-03-06 01:49:13 -0300

    more accs

---
+++ generators/plopfile.js
@@ -0,0 +1,32 @@
+export default function (plop) {
+  plop.setGenerator('component', {
+      description: 'application component logic',
+      prompts: [{
+          type: 'input',
+          name: 'name',
+          message: 'component name please'
+      }],
+      actions: [
+        {
+          type: 'add',
+          path: '../src/components/{{pascalCase name}}/index.tsx',
+          templateFile: 'templates/index.tsx.hbs'
+        },
+        {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/stories.tsx',
+        templateFile: 'templates/stories.tsx.hbs'
+        },
+        {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/styles.ts',
+        templateFile: 'templates/styles.ts.hbs'
+        },
+        {
+        type: 'add',
+        path: '../src/components/{{pascalCase name}}/testes.tsx',
+        templateFile: 'templates/testes.tsx.hbs'
+        },
+      ]
+  });
+};
